3dde37262ec12e2a1deded7af8b72869689b4da7
Artur Siery <dotintegral@gmail.com>
2020-03-01 15:37:20 +0100

    Reorganised test files

--- src/option/creators.test.ts
+++ src/option/creators.test.ts
@@ -1,7 +1,6 @@
-import { pipe } from 'fp-ts/lib/pipeable';
 import option, { None } from './option';
 
-describe('option', () => {
+describe('option::creators', () => {
   describe('just()', () => {
     it('should return proper structure', () => {
       const value = { a: 'a' };
@@ -80,85 +79,6 @@ describe('option', () => {
       const expected = option.none();
 
       expect(testO).toEqual(expected);
-    });
-  });
-
-  describe('map()', () => {
-    it('should map value correctly', () => {
-      const valueO = option.create(2);
-      const expectedO = option.create(4);
-
-      const resultO = pipe(
-        valueO,
-        option.map(x => x * 2)
-      );
-
-      expect(resultO).toEqual(expectedO);
-    });
-
-    it('should return none if mapped result is null', () => {
-      const valueO = option.create(2);
-      const expectedO = option.create(null);
-
-      const resultO = pipe(
-        valueO,
-        option.map(() => null)
-      );
-
-      expect(resultO).toEqual(expectedO);
-    });
-
-    it('should return none if mapped result is undefined', () => {
-      const valueO = option.create(2);
-      const expectedO = option.create(undefined);
-
-      const resultO = pipe(
-        valueO,
-        option.map(() => undefined)
-      );
-
-      expect(resultO).toEqual(expectedO);
-    });
-
-    it('should persist reason if mapping none with a reason', () => {
-      const valueO = option.create(null as unknown, 'error');
-      const expectedO = option.none('error');
-
-      const resultO = pipe(
-        valueO,
-        option.map(() => 'value')
-      );
-
-      expect(resultO).toEqual(expectedO);
-    });
-
-    it('should have no reason if mapping none without a reason', () => {
-      const valueO = option.create(null as unknown);
-      const expectedO = option.none();
-
-      const resultO = pipe(
-        valueO,
-        option.map(() => 'value')
-      );
-
-      expect(resultO).toEqual(expectedO);
-    });
-
-    it('should catch error in mapper and return none', () => {
-      const valueO = option.create('value');
-      const expectedO = option.none();
-
-      const resultO = pipe(
-        valueO,
-        option.map(() => {
-          throw new Error('an error');
-
-          // eslint-disable-next-line no-unreachable
-          return 'new-value';
-        })
-      );
-
-      expect(resultO).toEqual(expectedO);
     });
   });
 });
